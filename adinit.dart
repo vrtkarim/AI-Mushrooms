0638555173a56c54de0befcebbf60bc4b57661d6
Karim El Yaagoubi <109855615+vrtkarim@users.noreply.github.com>
2023-07-11 18:51:27 +0100

    Update adinit.dart

--- adinit.dart
+++ adinit.dart
@@ -3,8 +3,7 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 class Adinit {
   static BannerAd banner = BannerAd(
       size: AdSize.banner,
-      //ca-app-pub-1082671244415442/8652708889
-      adUnitId: 'ca-app-pub-1082671244415442/6851486133',
+      adUnitId: 'xxxxxxxxxxxxxxxxxxxxxxxxxx',
       listener: BannerAdListener(),
       request: AdRequest());
 }
